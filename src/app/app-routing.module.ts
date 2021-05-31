b93251805f30bad22979acf70ac751a7d73cdccc
Marcos Paulo <marcos.p.mcruz@outlook.com>
2021-05-30 21:47:01 -0300

    construção da tela inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntradaComponent } from './components/template/entrada/entrada.component';
+import { CadastroComponent } from './views/cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:EntradaComponent
+  },
+  {
+    path:'views/cadastro',
+    component:CadastroComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { EntradaComponent } from './components/template/entrada/entrada.component';
+import { LoginComponent } from './components/template/login/login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { CadastroComponent } from './views/cadastro/cadastro.component';
+import { MainComponent } from './components/template/main/main.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EntradaComponent,
+    LoginComponent,
+    FooterComponent,
+    CadastroComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
